f5cf76e92c47747248cc1f51a351bf1dc49ab35f
KHAN MAHMUD ARIF RABBANI <snigdho70@gmail.com>
2025-02-07 07:19:24 +0600

    ..

--- module02/practiceTernary.js
+++ module02/practiceTernary.js
@@ -1,13 +1,24 @@
+// Write a code is a student collegiate or non-collegiate. If one have more than or equal to 75% then he/she is a collegiate student otherwise non-collegiate..
+
+var attendance =75;
+
+var status = attendance >= 75 ? 'collegiate' : 'non-collegiate';
+
+console.log("Status:", status);
+
+
+
+
 // collegiate or non-collagiate
-// if a student get more then or equal to 75% then he/she is a collegiate student else he/she is non-collegiate student. write the code.. using ternary..
+// if a student get more then or equal to 75% then he/she is a collegiate student else he/she is non-collegiate student.For a normal collegiate student the form fillup fee is 1500 taka.. If he/she is non-collegiate charge a additional fee of 500 taka.  write the code.. using ternary..
 
 
 // Solution:
 
+
 var formFillUpAmount = 1500;
 var additionalFee = 500;
-var attendance = 72;
 
-var totalFee = attendance >= 75 ? formFillUpAmount : formFillUpAmount + additionalFee;
+var totalFee = status === 'collegiate' ? formFillUpAmount : formFillUpAmount + additionalFee;
 
 console.log(totalFee);
