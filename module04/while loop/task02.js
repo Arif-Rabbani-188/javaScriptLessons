1459ff0ffe95805e032347e3fc27b637a1140b23
KHAN MAHMUD ARIF RABBANI <snigdho70@gmail.com>
2025-02-09 18:28:43 +0600

    while loop complete

---
+++ module04/while loop/task02.js
@@ -0,0 +1,20 @@
+/*  Subtask-1: Find all the odd numbers from 61 to 100.  */
+
+let i = 61;
+
+while (i <= 100) {
+  console.log(i);
+  i += 2;
+}
+
+/*  Subtask-2: Find all the even numbers from 78 to 98.  */
+
+console.log("Task for even number");
+i = 78;
+
+while (i <= 98) {
+  console.log(i);
+  i += 2;
+}
+
+/*programming hero*/
