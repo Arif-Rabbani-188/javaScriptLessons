596bc21514309184c1b1439498c07df3a451918e
KHAN MAHMUD ARIF RABBANI <snigdho70@gmail.com>
2025-02-13 23:29:05 +0600

    object task01 is complete

---
+++ module05_object_strings/Object task/task01.js
@@ -0,0 +1,12 @@
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+// Access the golden rod color value in output.
+
+let property = colors["golden rod"];
+
+console.log(property);
