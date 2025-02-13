f58ecbd5c3a47bee9a9503b6b1ea6d705f74f85f
KHAN MAHMUD ARIF RABBANI <snigdho70@gmail.com>
2025-02-13 21:58:59 +0600

    Function task01 is complete

---
+++ module06_function/task01.js
@@ -0,0 +1,10 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(num1, num2, num3, num4) {
+  let result = num1 * num2 * num3 * num4;
+  return result;
+}
+
+let total = multiply(2, 3, 4, 5);
+
+console.log("Total is:", total);
