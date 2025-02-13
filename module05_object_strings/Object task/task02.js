480fcd02351dc617b7b42957c756f5315e835841
KHAN MAHMUD ARIF RABBANI <snigdho70@gmail.com>
2025-02-13 23:41:07 +0600

    object task02 and 03 is completed

---
+++ module05_object_strings/Object task/task02.js
@@ -0,0 +1,11 @@
+// For this object below add a property named passenger capacity with value 5
+
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car.registration = "2025"; //creating a new property in the car object
+
+console.log(car);
